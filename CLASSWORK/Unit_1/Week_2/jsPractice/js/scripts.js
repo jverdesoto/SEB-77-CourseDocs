b313fa98303a08b74cf0831ed80f28af54346a30
alfphiee <alfiebinnie@gmail.com>
2023-12-04 17:08:44 +0000

    JS Practice created - experimented with JS - before learning about animations

---
+++ CLASSWORK/Unit_1/Week_2/jsPractice/js/scripts.js
@@ -0,0 +1,288 @@
+// const myArr = [1, 2, 3, 4];
+
+// console.log(myArr);
+
+// for (let i = 0; i < myArr.length; i++) {
+//     console.log(myArr[i]);
+// }
+
+// myArr.forEach(ele => {
+//     console.log(ele);
+// });
+
+// myArr.map(ele => {
+//     console.log(ele);
+// })
+
+// const mySinglePost = {
+//     title: 'My Post Title',
+//     author: 'Rick',
+//     publishingDate: '2023-09-07',
+//     isDraft: false,
+//     content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime distinctio fuga, reiciendis dolore ducimus optio facilis, nostrum fugiat et repellendus voluptate quibusdam doloribus architecto quasi eveniet aliquid nam, aspernatur qui.',
+//     featuredImage: ['https://img1.com/img.jpg', 'https://img1.com/img-xl.jpg', 'https://img1.com/img-thumb.jpg']
+// };
+
+// console.log(mySinglePost);
+
+// const allPosts = [
+//   {
+//     title: "My Post Title 1",
+//     author: "Rick",
+//     publishingDate: "2023-09-07",
+//     isDraft: false,
+//     content:
+//       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime distinctio fuga, reiciendis dolore ducimus optio facilis, nostrum fugiat et repellendus voluptate quibusdam doloribus architecto quasi eveniet aliquid nam, aspernatur qui.",
+//     featuredImage: [
+//       "https://img1.com/img.jpg",
+//       "https://img1.com/img-xl.jpg",
+//       "https://img1.com/img-thumb.jpg",
+//     ],
+//   },
+//   {
+//     title: "My Post Title 2",
+//     author: "Rick",
+//     publishingDate: "2023-09-07",
+//     isDraft: true,
+//     content:
+//       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime distinctio fuga, reiciendis dolore ducimus optio facilis, nostrum fugiat et repellendus voluptate quibusdam doloribus architecto quasi eveniet aliquid nam, aspernatur qui.",
+//     featuredImage: [
+//       "https://img1.com/img.jpg",
+//       "https://img1.com/img-xl.jpg",
+//       "https://img1.com/img-thumb.jpg",
+//     ],
+//   },
+//   {
+//     title: "My Post Title 3",
+//     author: "Rick",
+//     publishingDate: "2023-09-07",
+//     isDraft: false,
+//     content:
+//       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime distinctio fuga, reiciendis dolore ducimus optio facilis, nostrum fugiat et repellendus voluptate quibusdam doloribus architecto quasi eveniet aliquid nam, aspernatur qui.",
+//     featuredImage: [
+//       "https://img1.com/img.jpg",
+//       "https://img1.com/img-xl.jpg",
+//       "https://img1.com/img-thumb.jpg",
+//     ],
+//   },
+//   {
+//     title: "My Post Title 4",
+//     author: "Rick",
+//     publishingDate: "2023-09-07",
+//     isDraft: false,
+//     content:
+//       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime distinctio fuga, reiciendis dolore ducimus optio facilis, nostrum fugiat et repellendus voluptate quibusdam doloribus architecto quasi eveniet aliquid nam, aspernatur qui.",
+//     featuredImage: [
+//       "https://img1.com/img.jpg",
+//       "https://img1.com/img-xl.jpg",
+//       "https://img1.com/img-thumb.jpg",
+//     ],
+//   },
+//   {
+//     title: "My Post Title 5",
+//     author: "Rick",
+//     publishingDate: "2023-09-07",
+//     isDraft: false,
+//     content:
+//       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime distinctio fuga, reiciendis dolore ducimus optio facilis, nostrum fugiat et repellendus voluptate quibusdam doloribus architecto quasi eveniet aliquid nam, aspernatur qui.",
+//     featuredImage: [
+//       "https://img1.com/img.jpg",
+//       "https://img1.com/img-xl.jpg",
+//       "https://img1.com/img-thumb.jpg",
+//     ],
+//   },
+//   {
+//     title: "My Post Title 6",
+//     author: "Rick",
+//     publishingDate: "2023-09-07",
+//     isDraft: false,
+//     content:
+//       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime distinctio fuga, reiciendis dolore ducimus optio facilis, nostrum fugiat et repellendus voluptate quibusdam doloribus architecto quasi eveniet aliquid nam, aspernatur qui.",
+//     featuredImage: [
+//       "https://img1.com/img.jpg",
+//       "https://img1.com/img-xl.jpg",
+//       "https://img1.com/img-thumb.jpg",
+//     ],
+//   },
+// ];
+
+// allPosts.map((post) => {
+//   console.log(post.title);
+// });
+
+const pinkFloyd = {
+  bandMembers: [
+    {
+      name: "Syd Barrett",
+      role: "Lead Guitarist",
+    },
+    {
+      name: "Roger Waters",
+      role: "Bassist",
+    },
+    {
+      name: "Nick Mason",
+      role: "Drummer",
+    },
+    {
+      name: "Rick Wright",
+      role: "Keyboardist",
+    },
+    {
+      name: "David Gilmour",
+      role: "Guitarist",
+    },
+  ],
+  discography: [
+    {
+        name: "The Wall",
+        year: 1979
+    },
+    {
+        name: "Wish you were Here",
+        year: 1975
+    },
+    {
+        name: "The Dark Side of the Moon",
+        year: 1973
+    },
+    {
+        name: "Pulse",
+        year: 1995
+    },
+    {
+        name: "Meddle",
+        year: 1971
+    },
+    {
+        name: "Atom Heart Mother",
+        year: 1970
+    },
+    {
+        name: 'The Division Bell',
+        year: 1994
+    },
+    {
+        name: 'The Final Cut',
+        year: 1983
+    },
+    {
+        name: 'Delicate Sound of Thunder',
+        year: 1988
+    },
+    {
+        name: 'Ummagumma',
+        year: 1969
+    }, 
+    {
+        name: 'A Momentary Lapse of Reason',
+        year: 1987
+    },
+    {
+        name: 'The Piper at the Gates of Dawn',
+        year: 1967
+    },
+    {
+        name: 'The Endless River',
+        year: 2014
+    },
+    {
+        name: 'Obscured By Clouds',
+        year: 1972
+    },
+    {
+        name: "Prism",
+        year: 1987
+    },
+    {
+        name: "A Saucerful of Secrets",
+        year: 1968
+    },
+    {
+        name: "More",
+        year: 1969
+    },
+    {
+        name: "Household Objects",
+        year: 1973
+    },
+    {
+        name: "Zabriskie Point",
+        year: 1970
+    },
+    {
+        name: "Relics",
+        year: 1971
+    },
+    {
+        name: "A Nice Pair",
+        year: 1973
+    },
+    {
+        name: "Animals",
+        year: 1977
+    },
+    {
+        name: "A Collection of Great Dance Songs",
+        year: 1981
+    },
+    {
+        name: "Works",
+        year: 1983
+    },
+    {
+        name: "Shine On",
+        year: 1992
+    },
+    {
+        name: "Oh By The Way",
+        year: 2007
+    }
+  ]
+};
+
+
+function findYearOfAlbum(albumName) {
+    let year;
+    pinkFloyd.discography.map(album => {
+        if(album.name.toLocaleLowerCase() == albumName.toLowerCase()) {
+            year = album.year;
+        }
+    })
+    return year;
+}
+
+function findRoleOfMember(memberName) {
+    let role;
+    pinkFloyd.bandMembers.map(member => {
+        if(member.name.toLocaleLowerCase() == memberName.toLowerCase()) {
+            role = member.role;
+        }
+    })
+    return year;
+}
+
+// function stringCompare(str1, str2) {
+//     return str1.toLowerCase() == str2.toLowerCase();
+// }
+
+// !Tests
+
+// *Albums
+
+console.log(findYearOfAlbum('The Wall')) // 1979
+console.log(findYearOfAlbum('the wall')) // 1979
+
+console.log(findYearOfAlbum('Wish you were Here')) // 1975
+console.log(findYearOfAlbum('The Dark Side of the Moon')) // 1973
+console.log(findYearOfAlbum('Pulse')) // 1995
+console.log(findYearOfAlbum('meddle')) // 1971
+
+console.log(findYearOfAlbum('Hey Hey')) // undefined
+
+
+// *Members
+
+console.log(findRoleOfMember('Syd barrett')) // Lead Guitarist
+console.log(findRoleOfMember('Roger Waters')) // Bassist
+
