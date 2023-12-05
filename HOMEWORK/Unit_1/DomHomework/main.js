d343895c43f5eb9797da60a65264c6bc046df46e
sdg877 <sdrakegill@gmail.com>
2023-12-05 06:58:02 +0000

    DOM Homework 4th Dec

---
+++ HOMEWORK/Unit_1/DomHomework/main.js
@@ -0,0 +1,21 @@
+// function createCards(arr) {
+//   const appDiv = document.getElementById("app");
+
+//   arr.forEach(post => {
+//       const card = document.createElement("div");
+//       card.classList.add("col-3", "mb-3");
+
+//        card.innerHTML = `
+//           <div class="card" style="width: 18rem;">
+//               <img src="${post.cover}" class="card-img-top" alt="...">
+//               <div class="card-body">
+//                   <h5 class="card-title">${post.title}</h5>
+
+//               </div>
+//           </div>
+//       `;
+//           appDiv.appendChild(card);
+// });
+// }
+
+// createCards(data);
