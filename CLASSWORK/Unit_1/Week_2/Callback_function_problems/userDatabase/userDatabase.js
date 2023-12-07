4d631c531e59c4ac817a7cdc9cf80aecfb9d8383
Alex <alexstocking@hotmail.co.uk>
2023-12-07 11:57:06 +0000

    Callback Function Problems

---
+++ CLASSWORK/Unit_1/Week_2/Callback_function_problems/userDatabase/userDatabase.js
@@ -0,0 +1,35 @@
+/**
+ * This UserDatabase object represents a list of all our rewards program users.
+ * 
+ * Can you find and fix the bugs so that the expected behavior is achieved?
+ */
+
+const UserDatabase = {
+  users: [],
+
+  // SOLUTION: this keyword is not bound to the UserDatabase since this was an arrow function
+  addUser(name, id, phone) {
+    this.users.push({ name, id , phone, hasCoupon: false, isEnrolledInRewards: false});
+  },
+
+  // what is wrong with this function?
+  loadFromFile() {
+    const usersFromFile = require("../../util/userDatabase/usersdb.json");
+    this.users = usersFromFile;
+  },
+  
+};
+
+module.exports = UserDatabase;
+
+// you can use these console logs to help you debug addUser()
+console.log(UserDatabase.users);
+UserDatabase.addUser("John Doe", 1234, "+1 (111) 111-1111");
+console.log(UserDatabase.users);
+
+
+// you can use these console logs to help you debug loadFromFile()
+UserDatabase.loadFromFile();
+console.log(UserDatabase.users);
+
+
